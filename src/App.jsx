50ad94b9f8ba678a5920f430660338b0a8d78b12
Nora Petrova <nora.petrova@prolific.com>
2025-06-06 17:21:41 +0100

    fix: Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// 1. Change BrowserRouter to HashRouter
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import GameFrame from "./components/GameFrame";
 import NumberFlow from "./games/NumberFlow";
 
 function App() {
   return (
-    <Router basename="/puzzle-games">
+    // 2. Remove the basename prop
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route
